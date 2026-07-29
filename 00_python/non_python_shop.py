70cfca71f3c26e0a3c8bad87d32bc36a38c7bbeb
Sonam Narula <sonamnarula@example.com>
2026-07-29 18:51:23 +0530

    Add 00_python examples

---
+++ 00_python/non_python_shop.py
@@ -0,0 +1,7 @@
+class Chai:
+    def __init__(self, sweetness, milk_level):
+        self sweetness = sweetness
+        self.milk_level = milk_level
+
+    def sip(self):
+        print("sipping chai")
