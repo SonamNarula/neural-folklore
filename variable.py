59b669278ece9140f3ca964f6912aec3633e5335
sonamnarula <sonamnarula@example.com>
2026-07-30 15:51:26 +0530

    Initial commit

---
+++ variable.py
@@ -0,0 +1,24 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as the value that it contains
+
+
+# strings
+first_name = "Sonam"
+food = "Pizza"
+email = "sonam@example.com"
+
+
+# integer
+age = 25
+quantity = 3
+num_of_students = 30
+
+# float
+price = 9.99
+gpa = 3.5
+distance = 12.5
+
+
+# boolean
+is_hungry = True
+is_sleepy = False
