f1e3210d27c8dfe106a072f58175ef03095d213b
sonamnarula <sonamnarula@example.com>
2026-08-05 17:45:24 +0530

    Add more Python problems

---
+++ PYTHON/problem3.py
@@ -0,0 +1,7 @@
+# Write a Python program to add two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+
+print("Sum =", sum)
---
+++ PYTHON/problem8.py
@@ -0,0 +1,7 @@
+
+# Write a Python program to calculate the square of a number entered by the user.
+number = float(input("Enter a number: "))
+
+square = number ** 2
+
+print("Square =", square)
