240cdda8201f66e4125683909948c355a243b999
SonamNarula <sonamnarula2108@gmail.com>
2026-08-25 09:38:59 +0530

    Add Day 1 LLM API integration

---
+++ week1/day1_hello_llm.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client = Groq(api_key=my_api_key)
+
+model = "openai/gpt-oss-20b"
+
+role = "user"
+prompt = "Do you know Padho with Pratyush?"
+
+message = {
+    "role": role,
+    "content": prompt
+}
+
+messages = [message]
+
+response = client.chat.completions.create(
+    model=model,
+    messages=messages
+)
+
+print(response.choices[0].message.content)
